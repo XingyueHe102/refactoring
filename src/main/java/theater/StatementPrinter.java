27737b8b44ec7aca1a73cf6f46704b6e9184e6e8
XingyueHe102 <xingyue.he@mail.utoronto.ca>
2025-11-14 12:53:45 +0800

    Task2.3 removing the frmt

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -4,9 +4,6 @@ import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Map;
 
-/**
- * This class generates a statement for a given invoice of performances.
- */
 public class StatementPrinter {
     private final Invoice invoice;
     private final Map<String, Play> plays;
@@ -16,47 +13,41 @@ public class StatementPrinter {
         this.plays = plays;
     }
 
-    /**
-     * Returns a formatted statement of the invoice associated with this printer.
-     * @return the formatted statement
-     * @throws RuntimeException if one of the play types is not known
-     */
     @SuppressWarnings({"checkstyle:MagicNumber", "checkstyle:FinalLocalVariable", "checkstyle:SuppressWarnings", "checkstyle:NeedBraces", "checkstyle:LineLength"})
     public String statement() {
         int totalAmount = 0;
         int volumeCredits = 0;
         StringBuilder result = new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
 
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (Performance p : invoice.getPerformances()) {
             Play play = plays.get(p.getPlayID());
-
             int thisAmount = getAmount(p);
 
-            // add volume credits
             volumeCredits += getVolumeCredits(p, play);
 
-            // print line for this order
-            result.append(String.format("  %s: %s (%s seats)%n", play.getName(), frmt.format(thisAmount / 100), p.getAudience()));
+            result.append(String.format("  %s: %s (%s seats)%n",
+                    play.getName(),
+                    usd(thisAmount),
+                    p.getAudience()));
+
             totalAmount += thisAmount;
         }
-        result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / 100)));
+
+        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
+
         return result.toString();
     }
 
     private static int getVolumeCredits(Performance performance, Play play) {
         int result = 0;
         result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-        // add extra credit for every five comedy attendees
         if ("comedy".equals(play.getType())) {
             result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
         }
         return result;
     }
 
-    @SuppressWarnings({"checkstyle:FinalLocalVariable", "checkstyle:SuppressWarnings"})
     private int getAmount(Performance performance) {
         Play play = plays.get(performance.getPlayID());
         int result = 0;
@@ -67,18 +58,24 @@ public class StatementPrinter {
                     result += 1000 * (performance.getAudience() - 30);
                 }
                 break;
+
             case "comedy":
                 result = Constants.COMEDY_BASE_AMOUNT;
                 if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
-                            + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                            + Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD);
                 }
                 result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
                 break;
+
             default:
                 throw new RuntimeException(String.format("unknown type: %s", play.getType()));
         }
         return result;
+    }
+
+    private String usd(int amount) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(amount / 100.0);
     }
 }
